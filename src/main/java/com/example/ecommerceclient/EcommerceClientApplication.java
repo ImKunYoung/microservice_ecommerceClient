c2f49e2a283eb5b16888a0306c6af254c2d486f9
ImKunYoung <lim8667@gmail.com>
2022-09-15 16:03:25 +0900

    init commit

--- src/main/java/com/example/ecommerceclient/EcommerceClientApplication.java
+++ src/main/java/com/example/ecommerceclient/EcommerceClientApplication.java
@@ -2,8 +2,10 @@ package com.example.ecommerceclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EcommerceClientApplication {
 
     public static void main(String[] args) {
